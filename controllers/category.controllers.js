bb3e177187cc43dfea9e6c1598eae78f1a3814a6
Anubhav Shavaran <anubhavshavaran@gmail.com>
2025-03-31 22:52:12 +0530

    1. Now the get all categories endpoint sends the count of subcategories.

--- controllers/category.controllers.js
+++ controllers/category.controllers.js
@@ -4,7 +4,15 @@ const prisma = new PrismaClient();
 
 const getAllCategories = async (req, res) => {
     try {
-        const categories = await prisma.category.findMany();
+        const categories = await prisma.category.findMany({
+            include: {
+                _count: {
+                    select: {
+                        SubCategory: true
+                    }
+                }
+            }
+        });
 
         res.status(200).json({
             success: true,
