4d45efa8ecb7dc0454ac7e2d2f530d628ebc4090
Anubhav Shavaran <anubhavshavaran@gmail.com>
2025-05-02 21:45:22 +0530

    1. Made the Redis URL for connection dynamic.

--- redis/redis.js
+++ redis/redis.js
@@ -1,6 +1,8 @@
 import {createClient} from 'redis';
 
-const redis = await createClient();
+const redis = await createClient({
+    url: process.env.REDIS_URL,
+});
 
 redis.on('error', (err) => console.log(err));
 redis.on('ready', () => console.log('redis is ready'));
