2b71fa6352bdf1e79e8b0f095e3375b5ed7c5ca2
Anubhav Shavaran <anubhavshavaran@gmail.com>
2025-05-21 19:49:16 +0530

    1. Made the redirect URL configurable.

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(urlencoded({extended: true}));
 passport.use(new Strategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CLIENT_CALLBACK_URL,
 }, (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
 }));
